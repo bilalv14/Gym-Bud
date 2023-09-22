f2dcb22cc71a8978f44672edca9b5056d23add07
Jacob Crews <jtcrews001@gmail.com>
2023-09-22 11:59:30 -0400

    Adding inital code to the repository.

---
+++ FitnessGym-Bud/src/main/java/com/CSC340f23/FitnessGymBud/FitnessGymBudApplication.java
@@ -0,0 +1,13 @@
+package com.CSC340f23.FitnessGymBud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessGymBudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessGymBudApplication.class, args);
+	}
+
+}
---
+++ FitnessGym-Bud/src/test/java/com/CSC340f23/FitnessGymBud/FitnessGymBudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340f23.FitnessGymBud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessGymBudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

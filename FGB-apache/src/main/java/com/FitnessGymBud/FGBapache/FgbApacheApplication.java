1faa6150ff117ea2b48b7e700ac4014ff4701a47
Bilal Zahid <muhammadbzahid@gmail.com>
2023-11-09 17:18:53 -0500

    Add files via upload

---
+++ FGB-apache/src/main/java/com/FitnessGymBud/FGBapache/FgbApacheApplication.java
@@ -0,0 +1,13 @@
+package com.FitnessGymBud.FGBapache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgbApacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgbApacheApplication.class, args);
+	}
+
+}
---
+++ FGB-apache/src/test/java/com/FitnessGymBud/FGBapache/FgbApacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FitnessGymBud.FGBapache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgbApacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

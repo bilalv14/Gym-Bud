1a789b7e15e67074b20b1ac4a672f342b9ab4acc
Bilal Zahid <muhammadbzahid@gmail.com>
2023-11-09 12:15:57 -0500

    Add files via upload

---
+++ FGB/src/test/java/com/FitnessGymBud/FGB/FgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FitnessGymBud.FGB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
